56d9ef4e4b1c544936cab79c0d19ce61207d02ff
Tim Hilt <thilt@in-technology.eu>
2022-08-29 08:11:45 +0200

    main: Fixing tsc compiling

--- services/orchestrator/src/index.tsx
+++ services/orchestrator/src/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { aFunc } from "@workspaces/a";
+import { bFunc } from "@workspaces/b";
+
+aFunc();
+bFunc();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
