40ee93bd5f6dc8f0ab9dac0c92900e7d5d6aab42
Tim Hilt <thilt@in-technology.eu>
2022-09-20 13:45:17 +0200

    main: Even cleaner slate

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "services",
+  root: "services/orchestrator",
   plugins: [react()],
 });
